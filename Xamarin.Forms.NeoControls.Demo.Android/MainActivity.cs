04bd05678080e1ff55c5e581844c11f7413350db
Felipe Baltazar <felipe.dasilvabaltazar@gmail.com>
2021-01-25 16:12:54 -0300

    ✨ Demo project created

---
+++ Xamarin.Forms.NeoControls.Demo.Android/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace Xamarin.Forms.NeoControls.Demo.Droid
+{
+    [Activity(Label = "Xamarin.Forms.NeoControls.Demo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ Xamarin.Forms.NeoControls.Demo/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using Xamarin.Forms.NeoControls;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: PreserveNeoControls]
